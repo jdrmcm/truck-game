8c29cf9eb0d84640d8dae47b97e07841ec292cac
jdrmcm <jordyn@prozac.dev>
2026-01-27 16:21:52 -0800

    new vehicle system

---
+++ raycast_vehicle.gd
@@ -0,0 +1,82 @@
+class_name RaycastVehicle
+extends RigidBody3D
+
+@export var wheels: Array[RaycastWheel]
+@export var acceleration := 600.0
+@export var max_speed := 20.0
+@export var accel_curve: Curve
+@export var tire_turn_speed: float = 1.0
+@export var tire_max_turn_degrees := 40.0
+@export var use_skid_marks := false
+@export var pneumatic_trail := 0.03
+@export var steering_return_damping := 0.5
+
+@export var skid_marks: Array[GPUParticles3D]
+
+@onready var total_wheels := wheels.size()
+
+var motor_input := 0
+var handbrake := false
+var is_slipping := false
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("brake"):
+		handbrake = true
+		is_slipping = true
+	elif event.is_action_released("brake"):
+		handbrake = false
+		is_slipping = false
+	motor_input = Input.get_action_strength("back") - Input.get_action_strength("forward")
+
+
+func _get_point_velocity(point: Vector3) -> Vector3:
+	return linear_velocity + angular_velocity.cross(point - global_position)
+
+
+func _basic_steering_rotation(wheel: RaycastWheel, delta: float) -> void:
+	if not wheel.is_steer: return
+	
+	var turn_input := Input.get_axis("right", "left") * tire_turn_speed
+	
+	var self_aligning_torque := 0.0
+	
+	var tire_vel := _get_point_velocity(wheel.wheel.global_position)
+	var lateral_vel := wheel.global_basis.z.dot(tire_vel)
+	
+	var slip_angle := atan2(lateral_vel, -global_basis.z.dot(tire_vel))
+	
+	self_aligning_torque = -slip_angle * pneumatic_trail * 100.0
+	
+	if turn_input:
+		wheel.rotation.y = clampf(wheel.rotation.y + turn_input * delta, 
+			deg_to_rad(-tire_max_turn_degrees), deg_to_rad(tire_max_turn_degrees))
+	else:
+		wheel.rotation.y = clampf(wheel.rotation.y + self_aligning_torque * delta,
+			deg_to_rad(-tire_max_turn_degrees), deg_to_rad(tire_max_turn_degrees))
+
+
+func _physics_process(delta: float) -> void:
+	var id := 0
+	var grounded := false
+	for wheel in wheels:
+		wheel.apply_wheel_physics(self)
+		_basic_steering_rotation(wheel, delta)
+		
+		
+		# skid marks
+		if use_skid_marks:
+			skid_marks[id].global_position = wheel.get_collision_point() + Vector3.UP * 0.01
+			skid_marks[id].look_at(skid_marks[id].global_position + global_basis.z)
+			
+			if not handbrake and wheel.grip_factor < 0.2:
+				is_slipping = false
+				skid_marks[id].emitting = false
+			
+			if handbrake and not skid_marks[id].emitting:
+				skid_marks[id].emitting = true
+		
+		if wheel.is_colliding():
+			grounded = true
+		
+		id += 1
---
+++ raycast_wheel.gd
@@ -0,0 +1,99 @@
+class_name RaycastWheel
+extends RayCast3D
+
+@export_group("Wheel properties")
+@export var spring_strength := 100.0
+# d=z(2Sqrt[k*m]), z=0.1, m=1, k=100 is the formula for optimal spring damping. K is spring strength and M is car mass in kg.
+# Z value should be between 0.1-1.0. 0.1-0.2 for more arcadey feel, 0.2-1.0 for more realistic.
+@export var spring_damping  := 2.0
+@export var rest_dist       := 0.5
+@export var over_extend     := 0.0
+@export var wheel_radius    := 0.4
+@export var z_traction      := 0.05
+
+@export_category("Motor")
+@export var is_motor := false
+@export var is_steer := false
+@export var grip_curve: Curve
+
+@onready var wheel: Node3D = get_child(0)
+
+var engine_force := 0.0
+var grip_factor  := 0.0
+
+
+func _ready() -> void:
+	target_position.y = -(rest_dist + wheel_radius + over_extend)
+
+
+func apply_wheel_physics(car: RaycastVehicle) -> void:
+	force_raycast_update()
+	target_position.y = -(rest_dist + wheel_radius + over_extend)
+	
+	
+	# rotate wheel mesh
+	var forward_dir     := -global_basis.z
+	var vel             := forward_dir.dot(car.linear_velocity)
+	wheel.rotate_x( (-vel * get_physics_process_delta_time()) / wheel_radius )
+	
+	
+	if not is_colliding(): return
+	# this line onwards the wheel is colliding
+	
+	
+	var contact         := get_collision_point()
+	var spring_len      := global_position.distance_to(contact) - wheel_radius
+	var offset          := rest_dist - spring_len
+	
+	wheel.position.y = -spring_len
+	contact = wheel.global_position
+	var force_pos       := contact - car.global_position
+	
+	
+	# spring forces
+	var spring_force    := spring_strength * offset
+	var tire_vel        := car._get_point_velocity(contact)
+	var spring_damp_f   := spring_damping * global_basis.y.dot(tire_vel)
+	
+	var y_force         := (spring_force - spring_damp_f) * get_collision_normal()
+	
+	
+	# accelerations
+	if is_motor and car.motor_input:
+		var speed_ratio := vel / car.max_speed
+		var accel       := car.accel_curve.sample_baked(speed_ratio)
+		var accel_force := forward_dir * car.acceleration * car.motor_input * accel
+		car.apply_force(accel_force, force_pos)
+	
+	
+	# tire x traction (steering)
+	var steering_x_vel  := global_basis.x.dot(tire_vel)
+	
+	grip_factor          = absf(steering_x_vel/tire_vel.length())
+	var x_traction      := grip_curve.sample_baked(grip_factor)
+	
+	if not car.handbrake and grip_factor < 0.2:
+		car.is_slipping = false
+	if car.handbrake:
+		x_traction = 0.01
+	elif car.is_slipping:
+		x_traction = 0.1
+	
+	var gravity         := -car.get_gravity().y
+	var x_force          = -global_basis.x * steering_x_vel * x_traction * ((car.mass * gravity) / car.total_wheels)
+	
+	# F = M * dV/T
+	#var desired_accel := (steering_x_vel * x_traction) / get_physics_process_delta_time()
+	#var x_force := -steer_side_dir * desired_accel * (mass/4.0)
+	
+	
+	# tire z traction
+	var f_vel           := forward_dir.dot(tire_vel)
+	var z_friction      := z_traction
+	var z_force          = global_basis.z * f_vel * z_friction * ((car.mass * gravity) / car.total_wheels)
+	
+	
+	# apply forces
+	car.apply_force(y_force, force_pos)
+	car.apply_force(x_force, force_pos)
+	car.apply_force(z_force, force_pos)
